847e953d1d642161ea6d376de8885dc098769209
tumanitochka <olegnastyle@yandex.ru>
2023-08-15 19:17:36 +0300

    *

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let message = 'Я JavaScript!';
-alert(message);
+// let message = 'Я JavaScript!';
+// alert('message');
+
+console.log('Привет из скрипта!')
+
+let web = 3
+var web2 = 4
+const web3 = 5
+console.log('web =',web,'web2 =',web2,'web3 =',web3)
+
+web = 6
+web2 = 7
+console.log('web =',web,'web2 =',web2,'web3 =',web3)
+
+let web4 = web + web3
+console.log(web,'+',web3,'=',web4)
