cc1e34bb581350b33b6d1adcdacaf4d3e9169946
tumanitochka <olegnastyle@yandex.ru>
2023-08-15 20:52:25 +0300

    *

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-let message = 'Все в консоли!';
-alert('message');
+// let message = 'Внимание! Срочно откройте консоль!'
+// alert(message)
 
-console.log('Привет из скрипта!')
+// console.log('Привет из скрипта!')
 
-let web = 3
-var web2 = 4
-const web3 = 5
-console.log('web =',web,'web2 =',web2,'web3 =',web3)
+// let web = 3
+// var web2 = 4
+// const web3 = 5
+// console.log('web =',web,'web2 =',web2,'web3 =',web3)
 
-web = 6
-web2 = 7
-console.log('web =',web,'web2 =',web2,'web3 =',web3)
+// web = 6
+// web2 = 7
+// console.log('web =',web,'web2 =',web2,'web3 =',web3)
 
-let web4 = web + web3
-console.log(web,'+',web3,'=',web4)
+// let web4 = web + web3
+// console.log(web,'+',web3,'=',web4)
+
+
+// let admin
+// let name = "Джон"
+//     admin = name
+
+// alert(admin)
+
+
+// let years = prompt('Какого Вы года рождения?')
+//     years = 2023 - years
+
+// alert('Ваш возраст: ' + years + '!')
+
+
+let one = Number(prompt('Введите первое число'))
+let calc = prompt('Введите символ:')
+let two = Number(prompt('Введите второе число'))
+
+console.log(eval(`${one}${calc}${two}`))
