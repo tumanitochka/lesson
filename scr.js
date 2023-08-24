ba6bea9147478e12e1621c09857257ac2e1e9553
tumanitochka <olegnastyle@yandex.ru>
2023-08-24 20:14:59 +0300

    *

--- scr.js
+++ scr.js
@@ -1,10 +1,36 @@
 // 1 задание
 
-let value = prompt('Какой объем Вашей флешки?') * 1024
-let result = (value / 820)
-alert('На вашей флешке поместится ' + Math.floor(result) + ' файлов, размером в 820 Мб')
+// let value = prompt('Какой объем Вашей флешки?') * 1024
+// let result = (value / 820)
+// alert('На вашей флешке поместится ' + Math.floor(result) + ' файлов, размером в 820 Мб')
 
 
 
 // 2 задание
+
+// let sum = +prompt("Cколько денег? ")
+// let price = prompt("Цена шоколадки? ")
+// let sell = Math.trunc(sum / price);
+// let change = sum - price * sell;
+// if (sell < 1) {
+//   alert('Денег не хватает');
+// }
+// else {
+//   alert('Получите свои ' + sell + ' шоколадки, а также сдачу ' + +change + ' рублей ');
+// }
+
+
+
+// 3 задание
+
+// number = prompt('Введите трехзначное число');
+// let a = Number(nmb % 10);
+// let b = Number((nmb - a) / 10 % 10);
+// let c = Number((nmb - a - b * 10) / 100);
+
+// // alert('Перевертыш: ' + (a * 100 + b * 10 + c));
+
+
+
+// 4 задание
 
