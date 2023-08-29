2b02b2e9e4efca884749e2e60d10dc8c57ae078c
tumanitochka <olegnastyle@yandex.ru>
2023-08-29 20:53:40 +0300

    *

--- less29.js
+++ less29.js
@@ -4,20 +4,24 @@
 // let b = String(a)
 
 
+
+
 // 2
 
 // let age = prompt('Введите ваш возраст')
 // let result = (age < 18) ? 'Школьник' : 'Студент'
 
+
+
+
 // 3
 
+// let a = 0
+// while (a < 10){
+//     let b = Math.floor(Math.random() * (999 - 100 + 1)) + 100
+//     console.log(b)
+//     a++
+// }
 
 
-let a = 0
-while (a < 10){
-    let b = Math.floor(Math.random() * (999 - 100 + 1)) + 100
-    console.log(b)
-    a++
-}
 
- 
