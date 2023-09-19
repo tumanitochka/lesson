221d23c423e27b2e6edbd30fb27e4e3be61a0d2e
tumanitochka <olegnastyle@yandex.ru>
2023-09-19 20:59:37 +0300

    *

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
-function min(a, b) {
-    return a < b ? a : b;
-  }
+// // 1 задание
+
+// const person = {
+//     age: 20,
+//     name: "Oleg",
+//     adress: {
+//         city: 'Sochi',
+//         street: 'Moskovskaja',
+//     }
+// }
+
+
+
+// // 2 задание
+
+// function info(){
+//     for (let key in person) {
+//         if(typeof person[key] === 'object'){
+//             subPerson = person[key];
+//             for (let subKey in subPerson) {
+//                 console.log(subPerson[subKey]);
+//             }
+//         }else  console.log(person[key]) 
+//     }
+// }
+
+// info()
+
+
+
+// // 3 задание
+
+
+
+// person.name = "Имя"
+
+
+
+// // 4 задание
+
+// const new_person = { ...person, age: '5', name: '6', adress: { city: '7', street: '8'} }
+
+
+
+
+console.log(λ)
