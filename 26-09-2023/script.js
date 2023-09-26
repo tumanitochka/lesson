e626dae12d47457c1c70f49b127b18ec4c139844
tumanitochka <olegnastyle@yandex.ru>
2023-09-26 19:28:05 +0300

    *

--- 26-09-2023/script.js
+++ 26-09-2023/script.js
@@ -1,6 +1,6 @@
 const user = { // создаеи объект user
     name: "John Doe", // создаем свойство name со значением "John Doe"
     age: 40 // создаем свойство age со значением 40
-};
+}
 
-console.log('rr' in user) // проверка переменной в объекте user
+console.log('key' in user) // проверка наличия свойства "key" в объекте user (true или false)
