51d89c24d081bb3f07390f85548b28c1063b3f8c
tumanitochka <olegnastyle@yandex.ru>
2023-09-26 19:22:19 +0300

    *

---
+++ 26-09-2023/script.js
@@ -0,0 +1,6 @@
+const user = { // создаеи объект user
+    name: "John Doe", // создаем свойство name со значением "John Doe"
+    age: 40 // создаем свойство age со значением 40
+};
+
+console.log('rr' in user) // проверка переменной в объекте user
--- script.js
+++ script.js
@@ -42,5 +42,3 @@
 
 
 
-
-console.log(λ)
