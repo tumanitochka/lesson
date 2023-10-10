3861bc53b331751422a459f38c9c74e1cd81f3a7
tumanitochka <olegnastyle@yandex.ru>
2023-10-10 21:25:30 +0300

    *

--- 03-10-23/script.js
+++ 03-10-23/script.js
@@ -5,23 +5,47 @@ let name = document.getElementById('name')
 let age = document.getElementById('age')
 let city = document.getElementById('city')
 let phone = document.getElementById('phone')
+let val = document.getElementById('val')
 
 
-// ложим значиния input в массив user
-function saveAdd(n, a, c, p) {
-    user.push(n)
-    user.push(a)
-    user.push(c)
-    user.push(p)
-}
+function saveAdd() {
+    user.push({name: name.value, age: age.value, city: city.value, phone: phone.value})
+                alert(`Данные о ${name.value} сохранены.`)
+                name.value = ''
+                age.value = ''
+                city.value = ''
+                phone.value = ''
+    }
 
-saveAdd(name.value, age.value, city.value, phone.value) // передаем значения в функцию
-
-
-
-
-// перебор элементов массива
-for (let i = 0; i < user.length; i++) {
-    var newDiv = document.createElement(li);
-    newDiv.innerHTML = user[i] 
-}
+    function showUsersName(){
+        val.value = ''
+        let buff = ''
+        user.forEach((e) => {
+            buff += '<li>' + e.name + '</li>'
+        })
+        val.innerHTML = buff
+    }
+    function showUsersAge(){
+        val.value = ''
+        let buff = ''
+        user.forEach((e) => {
+            buff += '<li>' + e.age + '</li>'
+        })
+        val.innerHTML = buff
+    }
+    function showUsersCity(){
+        val.value = ''
+        let buff = ''
+        user.forEach((e) => {
+            buff += '<li>' + e.city + '</li>'
+        })
+        val.innerHTML = buff
+    }
+    function showUsersPhone(){
+        val.value = ''
+        let buff = ''
+        user.forEach((e) => {
+            buff += '<li>' + e.phone + '</li>'
+        })
+        val.innerHTML = buff
+    }
