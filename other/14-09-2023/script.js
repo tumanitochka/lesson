830d33a27f8185da2a633bdf4919df8d8e466019
tumanitochka <olegnastyle@yandex.ru>
2023-10-31 18:29:53 +0300

    update

---
+++ other/14-09-2023/script.js
@@ -0,0 +1,10 @@
+// let obj = new Object();
+let user = {
+    name: "Oleg",
+    age: 28,
+    key: "value",
+
+}
+
+
+console.log(user.key)
---
+++ other/26-09-2023/script.js
@@ -0,0 +1,13 @@
+// const user = { // создаеи объект user
+//     name: "John Doe", // создаем свойство name со значением "John Doe"
+//     age: 40 // создаем свойство age со значением 40
+// }
+
+// console.log('key' in user) // проверка наличия свойства "key" в объекте user (true или false)
+
+
+
+const Matic = {} // создание объекта конструктора
+Matic.ran = Math.random // 
+
+alert(Math.trunc(Matic.ran(100)*100))
